Fix off by one error

circuits/sae_stats_collection.py
@@ -86,7 +86,7 @@ def get_ae_stats(autoencoder_path: str, save_results: bool = False) -> tuple[dic
         submodule,
         n_ctxs=512,
         ctx_len=context_length,
-        refresh_batch_size=4,
+        refresh_batch_size=batch_size,
         io="out",
         d_submodule=activation_dim,
         device=DEVICE,
@@ -116,8 +116,8 @@ def get_ae_stats(autoencoder_path: str, save_results: bool = False) -> tuple[dic
         dictionary=ae,
         dims=idx[:],
         n_inputs=5000,
-        k=TOP_K,
-        batch_size=10,
+        k=TOP_K + 1,
+        batch_size=batch_size,
         processing_device=torch.device("cpu"),
     )
 