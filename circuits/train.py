b8fcca9bdf5628fb0ce88e940f315b100fac0285
Adam Karvonen <adam.karvonen@gmail.com>
2024-05-01 15:03:20 -0500

    Updated training script

--- circuits/train.py
+++ circuits/train.py
@@ -1,5 +1,6 @@
 from nnsight import LanguageModel
 import torch
+import itertools
 
 from dictionary_learning import ActivationBuffer
 from dictionary_learning.training import trainSAE
@@ -13,9 +14,9 @@ tokenizer = NanogptTokenizer("models/meta.pkl")
 model = convert_nanogpt_model("models/lichess_8layers_ckpt_no_optimizer.pt", torch.device(DEVICE))
 model = LanguageModel(model, device_map=DEVICE, tokenizer=tokenizer).to(DEVICE)
 
-submodule = model.transformer.h[5].mlp  # layer 1 MLP
-activation_dim = 512  # output dimension of the MLP
-dictionary_size = 16 * activation_dim
+submodule = model.transformer.h[5]
+activation_dim = 512  # output dimension of the layer
+resample_steps = 50000
 
 
 data = hf_dataset_to_generator("adamkarvonen/chess_sae_text")
@@ -27,88 +28,34 @@ buffer = ActivationBuffer(
     ctx_len=256,
     refresh_batch_size=128,
     io="out",
-    d_submodule=512,
+    d_submodule=activation_dim,
     device=DEVICE,
 )
 
+learning_rates = [1e-4, 1e-3]
+l1_penalties = [1e-4, 1e-3, 1e-2, 3e-2, 6e-2, 1e-1, 3e-1]
+dictionary_sizes = [4 * activation_dim, 16 * activation_dim]
+
+param_combinations = list(itertools.product(learning_rates, l1_penalties, dictionary_sizes))
+
+trainer_configs = [
+    {
+        "trainer": StandardTrainer,
+        "lr": lr,
+        "l1_penalty": l1,
+        "warmup_steps": 2000,
+        "resample_steps": resample_steps,
+        "activation_dim": activation_dim,
+        "dictionary_size": dictionary_size,
+    }
+    for lr, l1, dictionary_size in param_combinations
+]
+
 trainSAE(
     buffer,
-    activation_dim=activation_dim,
-    dictionary_size=dictionary_size,
-    trainer_configs=[
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-4,
-            "l1_penalty": 1e-2,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-4,
-            "l1_penalty": 3e-2,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-4,
-            "l1_penalty": 6e-2,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-4,
-            "l1_penalty": 1e-1,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-4,
-            "l1_penalty": 3e-1,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-3,
-            "l1_penalty": 1e-2,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-3,
-            "l1_penalty": 3e-2,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-3,
-            "l1_penalty": 6e-2,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-3,
-            "l1_penalty": 1e-1,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-        {
-            "trainer": StandardTrainer,
-            "lr": 1e-3,
-            "l1_penalty": 3e-1,
-            "warmup_steps": 2000,
-            "resample_steps": 150000,
-        },
-    ],
+    trainer_configs=trainer_configs,
     steps=300000,
-    save_steps=150000,
+    save_steps=100000,
     save_dir="",
     log_steps=1000,
 )
