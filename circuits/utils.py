6b5f5c9885991b78ba4f85768c59c54de39a034a
rangell <ricocharlesangell@gmail.com>
2024-05-16 18:56:06 +0000

    fixed import of NNsightActivationBuffer

--- circuits/utils.py
+++ circuits/utils.py
@@ -12,7 +12,7 @@ from tqdm import tqdm
 from transformers import GPT2LMHeadModel
 from transformer_lens import HookedTransformer
 
-from circuits.nnsight_buffer import NNsightActivationBuffer
+from circuits.dictionary_learning.buffer import NNsightActivationBuffer
 from circuits.dictionary_learning import AutoEncoder
 from circuits.chess_utils import encode_string
 from circuits.dictionary_learning import ActivationBuffer
