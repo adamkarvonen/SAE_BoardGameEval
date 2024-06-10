36a57453922fd03c448b7c0d83011b69d107ca73
Adam Karvonen <adam.karvonen@gmail.com>
2024-06-10 16:06:02 -0500

    Add othello test

--- tests/test_full_pipeline.py
+++ tests/test_full_pipeline.py
@@ -3,19 +3,24 @@ import pandas as pd
 import circuits.full_pipeline as full_pipeline
 import circuits.pipeline_config as pipeline_config
 
+TEST_INPUTS = 25
+BATCH_SIZE = 5
+TOLERANCE = 1e-6
 
-def test_full_pipeline():
+
+def test_full_chess_pipeline():
 
     test_config = pipeline_config.Config(
-        eval_sae_n_inputs=25,
-        batch_size=5,
-        eval_results_n_inputs=25,
-        board_reconstruction_n_inputs=25,
+        eval_sae_n_inputs=TEST_INPUTS,
+        batch_size=BATCH_SIZE,
+        eval_results_n_inputs=TEST_INPUTS,
+        board_reconstruction_n_inputs=TEST_INPUTS,
         analysis_on_cpu=True,
+        precompute=True,
     )
     autoencoder_group_paths = ["autoencoders/testing_chess"]
-    csv_output_path = "autoencoders/testing_chess/results.csv"
-    final_output_path = "autoencoders/testing_chess/f1_results.csv"
+    csv_output_path = f"{autoencoder_group_paths[0]}/results.csv"
+    final_output_path = f"{autoencoder_group_paths[0]}/f1_results.csv"
 
     full_pipeline.analyze_sae_groups(autoencoder_group_paths, csv_output_path, test_config)
 
@@ -27,4 +32,31 @@ def test_full_pipeline():
 
     expected_result = 0.7595600485801697
 
-    assert actual_result - expected_result < 1e-6
+    assert actual_result - expected_result < TOLERANCE
+
+
+def test_full_othello_pipeline():
+
+    test_config = pipeline_config.Config(
+        eval_sae_n_inputs=TEST_INPUTS,
+        batch_size=BATCH_SIZE,
+        eval_results_n_inputs=TEST_INPUTS,
+        board_reconstruction_n_inputs=TEST_INPUTS,
+        analysis_on_cpu=True,
+        precompute=True,
+    )
+    autoencoder_group_paths = ["autoencoders/testing_othello"]
+    csv_output_path = f"{autoencoder_group_paths[0]}/results.csv"
+    final_output_path = f"{autoencoder_group_paths[0]}/f1_results.csv"
+
+    full_pipeline.analyze_sae_groups(autoencoder_group_paths, csv_output_path, test_config)
+
+    df = pd.read_csv(final_output_path)
+
+    actual_result = df[
+        "games_batch_to_state_stack_mine_yours_blank_mask_BLRRC_best_f1_score_per_class"
+    ].max()
+
+    expected_result = 0.928957581520081
+
+    assert actual_result - expected_result < TOLERANCE
