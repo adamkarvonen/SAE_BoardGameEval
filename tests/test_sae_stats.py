286c2b9d282e7e0a6478d5bc8937c5bb02243185
Adam Karvonen <adam.karvonen@gmail.com>
2024-06-11 11:34:06 -0500

    Remove outdated test

--- tests/test_sae_stats.py
+++
@@ -1,4 +0,0 @@
-# from circuits.sae_stats_collection import compute_all_ae_stats
-
-# Commented because this in outdated
-# compute_all_ae_stats("autoencoders/group0/", n_inputs=100, top_k=10, max_dims=100, batch_size=10)
